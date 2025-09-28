5cb4fcf960a554d41f104c32a99c10800e531462
d4gr4ss <dagrassitelli@gmail.com>
2025-09-28 16:20:16 +0200

    complete step 4 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,20 +5,23 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = total === 0 ? 0 : (good - bad) / total;
   const positive = total === 0 ? 0 : (good / total) * 100;
 
-  return (
-    <p>
-      good {good} <br />
-      neutral {neutral} <br />
-      bad {bad} <br />
-      all {total} <br />
-      avarage {average} <br />
-      positive {positive} %
-    </p>
-  );
+  if (total) {
+    return (
+      <p>
+        good {good} <br />
+        neutral {neutral} <br />
+        bad {bad} <br />
+        all {total} <br />
+        avarage {average} <br />
+        positive {positive} %
+      </p>
+    );
+  } else {
+    return <p>No feedback given</p>;
+  }
 };
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
